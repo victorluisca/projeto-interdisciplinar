f7c643fe56e1a9892c42908c03633f1b08d5d164
Gilmar <gilmarsoaresfranco@gmail.com>
2024-05-23 22:29:02 -0300

    Adicionado os partials

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./views/*.ejs"],
+  content: ["./views/**/*.ejs"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "cor-primaria": "#112D4E",
+        "cor-secundaria": "#3F72AF",
+        "cor-tercearia": "#DBE2EF",
+        "cor-quartearia": "#F9F7F7",
+      },
+    },
+    fontFamily: {
+      sans: ["Montserrat", "ui-sans-serif"],
+    },
   },
   plugins: [{ tailwindcss: {}, autoprefixer: {} }],
 };
