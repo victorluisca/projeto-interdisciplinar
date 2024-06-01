cc16a5bd1ca0a1d3a2576fa84b7e61f802d7494f
gilSFranco <gilmarsoaresfranco@gmail.com>
2024-05-31 22:27:30 -0300

    Tela de Principal e login terminadas

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
 const port = 8080;
 
 app.listen(port, (err) => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,12 @@ export default {
     extend: {
       colors: {
         "cor-primaria": "#112D4E",
+        "cor-primaria-variacao": "#173D6A",
         "cor-secundaria": "#3F72AF",
         "cor-tercearia": "#DBE2EF",
         "cor-quartearia": "#F9F7F7",
+        "cor-cinza": "#c0c0c0",
+        "cor-verde": "#24AB70",
       },
     },
     fontFamily: {
